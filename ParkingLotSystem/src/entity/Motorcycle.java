a6b5cc7d9adfcb218687ded85be042ec18968c4b
laijian398 <laijian2004@gmail.com>
2026-02-16 02:12:55 +0800

    Update Motorcycle.java

--- ParkingLotSystem/src/entity/Motorcycle.java
+++ ParkingLotSystem/src/entity/Motorcycle.java
@@ -5,7 +5,16 @@ import enums.SpotCategory;
 public class Motorcycle extends Vehicle {
 
     public Motorcycle(String plateNumber) {
-        super(plateNumber, SpotCategory.COMPACT, false);
+        super(plateNumber, false);
+    }
+
+    @Override
+    public SpotCategory getRequiredSpotType() {
+        return SpotCategory.COMPACT;
+    }
+
+    @Override
+    public String getVehicleType() {
+        return "Motorcycle";
     }
 }
-
