7837e13af5cf849c7c8d521c2c741b2cf2a74740
laijian398 <laijian2004@gmail.com>
2026-02-16 02:11:51 +0800

    Update Car.java

--- ParkingLotSystem/src/entity/Car.java
+++ ParkingLotSystem/src/entity/Car.java
@@ -5,6 +5,16 @@ import enums.SpotCategory;
 public class Car extends Vehicle {
 
     public Car(String plateNumber) {
-        super(plateNumber, SpotCategory.COMPACT, false);
+        super(plateNumber, false);
+    }
+
+    @Override
+    public SpotCategory getRequiredSpotType() {
+        return SpotCategory.COMPACT;
+    }
+
+    @Override
+    public String getVehicleType() {
+        return "Car";
     }
 }
