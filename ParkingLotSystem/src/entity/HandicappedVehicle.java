2f28a8d4183a5ac8817d3b7b86c802b0395fb26b
laijian398 <laijian2004@gmail.com>
2026-02-16 02:13:11 +0800

    Update HandicappedVehicle.java

--- ParkingLotSystem/src/entity/HandicappedVehicle.java
+++ ParkingLotSystem/src/entity/HandicappedVehicle.java
@@ -5,6 +5,16 @@ import enums.SpotCategory;
 public class HandicappedVehicle extends Vehicle {
 
     public HandicappedVehicle(String plateNumber) {
-        super(plateNumber, SpotCategory.HANDICAPPED, true);
+        super(plateNumber, true);
+    }
+
+    @Override
+    public SpotCategory getRequiredSpotType() {
+        return SpotCategory.HANDICAPPED;
+    }
+
+    @Override
+    public String getVehicleType() {
+        return "Handicapped";
     }
 }
